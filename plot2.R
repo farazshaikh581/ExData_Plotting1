244119817731d9f56ddf660a11b7f2a7e7fa7288
farazshaikh581 <34497246+farazshaikh581@users.noreply.github.com>
2020-06-14 18:45:51 +0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- data.table::fread(input = "household_power_consumption.txt", na.strings = "?")
+
+data[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+data[, Date_and_Time := as.POSIXct(paste(Date, Time), format = "%d%m%Y %H:%M:%S")]
+data <- data[(Date_and_Time >= "2007-02-01") & (Date_and_Time < "2007-02-03")]
+png("plot2.png", width = 480, height = 480)
+plot(x = data[, Global_active_power], y = data[, Date_and_Time], type = "l", xlab = "", ylab = "Global Active Power (Kilowatts")
