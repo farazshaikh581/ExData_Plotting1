ed9e4e0055313f63601fa72b7f843920b7210ae9
farazshaikh581 <34497246+farazshaikh581@users.noreply.github.com>
2020-06-14 18:03:08 +0500

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,7 @@
+File <- "./data/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subset <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+globalActivePower <- as.numeric(subset$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab = "Frequency)
